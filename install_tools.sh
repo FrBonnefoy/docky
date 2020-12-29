5eca02e2af4058ad060bdf3d6166170b5edeb7d2
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2020-12-29 18:51:48 +0100

    Add files via upload

---
+++ install_tools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y upgrade
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sudo sh get-docker.sh
+#sudo usermod -aG docker azureuser
+mkdir Dockerfile_
+cd Dockerfile_
+wget https://github.com/FrBonnefoy/docky/blob/main/initbook.sh
+wget https://github.com/FrBonnefoy/docky/blob/main/Dockerfile
+sudo docker build -t booking_url . 
+sudo docker pull httpd
+docker pull jupyter/all-spark-notebook
+docker pull jupyter/tensorflow-notebook
+
+#echo 'Finished installation'
+
