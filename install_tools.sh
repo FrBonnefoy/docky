65eabf6094dc1da3b72052715672c08a0ef0ab52
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2020-12-29 19:41:53 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -5,14 +5,12 @@ sudo apt -y upgrade
 #curl -fsSL https://get.docker.com -o get-docker.sh
 #sudo sh get-docker.sh
 #sudo usermod -aG docker azureuser
-mkdir booking_urls_docker
-mkdir luminati_docker
+mkdir booking_small_urls_docker
 mkdir booking_info_docker
 cd booking_urls_docker
 https://github.com/FrBonnefoy/docky/blob/main/urls/initbook.sh
 https://github.com/FrBonnefoy/docky/blob/main/urls/Dockerfile
 cd ..
-cd luminati_docker
 sudo docker build -t booking_url . 
 sudo docker pull httpd
 sudo docker pull jupyter/datascience-notebook
