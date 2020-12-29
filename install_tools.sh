0a5c9baea9bc26fb5572acb49c090fb14232d504
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2020-12-29 20:13:52 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -15,11 +15,13 @@ sudo docker build -t booking_url .
 cd ..
 sudo docker pull luminati/luminati-proxy
 sudo docker pull httpd
+sudo docker run -d -p 80:80/tcp -v /home/azureuser/apache:/usr/local/apache2/htdocs --name apache httpd
 sudo docker pull jupyter/datascience-notebook
 sudo docker pull jupyter/tensorflow-notebook
-mkdir data_science
-mkdir booking_url
-mkdir tensorflow
-mkdir booking
+mkdir /home/azureuser/apache/data_science
+mkdir /home/azureuser/apache/booking_small_url
+mkdir /home/azureuser/apache/tensorflow
+mkdir /home/azureuser/apache/booking_info
+mkdir /home/azureuser/apache/booking_big_url
 #echo 'Finished installation'
 
