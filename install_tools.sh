5a6823f8bca753b92728db7e11b3b30e8085f063
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2020-12-29 19:03:20 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -13,6 +13,9 @@ sudo docker build -t booking_url .
 sudo docker pull httpd
 sudo docker pull jupyter/datascience-notebook
 sudo docker pull jupyter/tensorflow-notebook
-
+mkdir data_science
+mkdir booking_url
+mkdir tensorflow
+mkdir booking
 #echo 'Finished installation'
 
