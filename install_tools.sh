6d89a62437cddb8a4217cadee18dd8030e011750
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2020-12-29 19:43:14 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -2,16 +2,16 @@
 
 sudo apt -y update
 sudo apt -y upgrade
-#curl -fsSL https://get.docker.com -o get-docker.sh
-#sudo sh get-docker.sh
-#sudo usermod -aG docker azureuser
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker azureuser
 mkdir booking_small_urls_docker
 mkdir booking_info_docker
 cd booking_urls_docker
-https://github.com/FrBonnefoy/docky/blob/main/urls/initbook.sh
-https://github.com/FrBonnefoy/docky/blob/main/urls/Dockerfile
-cd ..
+wget https://github.com/FrBonnefoy/docky/blob/main/small_urls/Dockerfile
+wget https://github.com/FrBonnefoy/docky/blob/main/small_urls/initbook.sh
 sudo docker build -t booking_url . 
+cd ..
 sudo docker pull httpd
 sudo docker pull jupyter/datascience-notebook
 sudo docker pull jupyter/tensorflow-notebook
