79d4c7de6a27b84866db6ca02085fe9ab53dbef1
Francisco <fp.ob>
2021-01-15 11:31:36 +0100

    streamlined firefox session

--- big_urls/support.py
+++ big_urls/support.py
@@ -102,18 +102,18 @@ def open_session_firefox():
     options.add_argument("--window-size=1024x768")
     #options.add_argument('start-maximized')
     profile = webdriver.FirefoxProfile()
-    profile.add_extension(current_path+"/disable_webrtc-1.0.23-an+fx.xpi")
+    #profile.add_extension(current_path+"/disable_webrtc-1.0.23-an+fx.xpi")
     #profile.add_extension(current_path+"/adblock_for_firefox-4.24.1-fx.xpi")
-    profile.add_extension(current_path+"/image_block-5.0-fx.xpi")
-    profile.add_extension(current_path+"/ublock_origin-1.31.0-an+fx.xpi")
+    #profile.add_extension(current_path+"/image_block-5.0-fx.xpi")
+    #profile.add_extension(current_path+"/ublock_origin-1.31.0-an+fx.xpi")
     profile.DEFAULT_PREFERENCES['frozen']["media.peerconnection.enabled" ] = False
     profile.set_preference("media.peerconnection.enabled", False)
     profile.set_preference("permissions.default.image", 2)
     profile.update_preferences()
     browser = webdriver.Firefox(profile,options=options)
-    browser.install_addon(current_path+"/disable_webrtc-1.0.23-an+fx.xpi", temporary=True)
-    browser.install_addon(current_path+"/image_block-5.0-fx.xpi", temporary=True)
-    browser.install_addon(current_path+"/ublock_origin-1.31.0-an+fx.xpi", temporary=True)
+    #browser.install_addon(current_path+"/disable_webrtc-1.0.23-an+fx.xpi", temporary=True)
+    #browser.install_addon(current_path+"/image_block-5.0-fx.xpi", temporary=True)
+    #browser.install_addon(current_path+"/ublock_origin-1.31.0-an+fx.xpi", temporary=True)
 
 
 def open_session():
