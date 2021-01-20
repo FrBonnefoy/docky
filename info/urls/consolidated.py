59d6da10b14c157e9844327082b0a879e78c09ad
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2021-01-20 11:12:26 +0100

    Update consolidated.py

--- info/urls/consolidated.py
+++ info/urls/consolidated.py
@@ -1,8 +1,8 @@
 import glob
 
 # Consolidate url file
-try:
-    read_files = glob.glob("booking_done_url*")
+
+read_files = glob.glob("booking_done_url*")
     with open("consolidatedurls.txt", "wb") as outfile:
         for f in read_files:
             with open(f, "rb") as infile:
@@ -10,15 +10,14 @@ try:
 
     #Import logs
 
-    flogname='consolidatedurls.txt'
-    with open(flogname) as flogdone:
-        done_urls=flogdone.readlines()
+flogname='consolidatedurls.txt'
+with open(flogname) as flogdone:
+    done_urls=flogdone.readlines()
+done_urls=list(filter(lambda x: 'https://www.booking.com/' in x, done_urls))
+done_urls=list(dict.fromkeys(done_urls))
 
-    done_urls=list(filter(lambda x: 'https://www.booking.com/' in x, done_urls))
-    done_urls=list(dict.fromkeys(done_urls))
-
-    with open("booking_url.txt", "w") as outfile:
-        for url in done_urls:
-            outfile.write(url)
+with open("booking_url.txt", "w") as outfile:
+    for url in done_urls:
+        outfile.write(url)
 except:
     pass
