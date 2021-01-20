eb48f737d68545b37d7691531d9886c257c6e921
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2021-01-20 11:39:59 +0100

    Update initbook.sh

--- info/initbook.sh
+++ info/initbook.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 python3 /Bookinfo/urls/consolidated.py
-nohup python3 -u /Bookinfo/booking.py > progressbar.log &
+cd /Bookinfo
+nohup python3 -u booking.py > progressbar.log &
 
 /bin/bash
