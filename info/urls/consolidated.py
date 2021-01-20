adcf44bb0035a9c9a2d9d9cf88360f1cadcb2b6a
Francisco Bonnefoy <37573809+FrBonnefoy@users.noreply.github.com>
2021-01-20 11:01:19 +0100

    Update consolidated.py

--- info/urls/consolidated.py
+++ info/urls/consolidated.py
@@ -1,22 +1,24 @@
 import glob
 
 # Consolidate url file
+try:
+    read_files = glob.glob("booking_done_url*")
+    with open("consolidatedurls.txt", "wb") as outfile:
+        for f in read_files:
+            with open(f, "rb") as infile:
+                outfile.write(infile.read())
 
-read_files = glob.glob("booking_done_url*")
-with open("consolidatedurls.txt", "wb") as outfile:
-    for f in read_files:
-        with open(f, "rb") as infile:
-            outfile.write(infile.read())
+    #Import logs
 
-#Import logs
+    flogname='consolidatedurls.txt'
+    with open(flogname) as flogdone:
+        done_urls=flogdone.readlines()
 
-flogname='consolidatedurls.txt'
-with open(flogname) as flogdone:
-    done_urls=flogdone.readlines()
+    done_urls=list(filter(lambda x: 'https://www.booking.com/' in x, done_urls))
+    done_urls=list(dict.fromkeys(done_urls))
 
-done_urls=list(filter(lambda x: 'https://www.booking.com/' in x, done_urls))
-done_urls=list(dict.fromkeys(done_urls))
-
-with open("booking_url.txt", "w") as outfile:
-    for url in done_urls:
-        outfile.write(url)
+    with open("booking_url.txt", "w") as outfile:
+        for url in done_urls:
+            outfile.write(url)
+except:
+    pass
